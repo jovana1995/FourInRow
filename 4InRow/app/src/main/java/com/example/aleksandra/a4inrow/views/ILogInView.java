3f3ccb2d8db5b37f6d5117142490841641bcb7d9
Jovana Stamenkovic <jovana.stamenkovic@elfak.rs>
2018-04-09 15:23:03 +0200

    app

--- 4InRow/app/src/main/java/com/example/aleksandra/a4inrow/views/ILogInView.java
+++
@@ -1,9 +0,0 @@
-package com.example.aleksandra.a4inrow.views;
-
-/**
- * Created by Aleksandra on 20/01/2018.
- */
-
-public interface ILogInView extends IBaseView {
-    void logInSuccessful(String s);
-}
